817f34256c0498185089bfa000b5fafbeec4faf1
oscartflores <oflores1502@gmail.com>
2025-07-10 14:37:20 -0700

    Completed player

--- main.py
+++ main.py
@@ -3,21 +3,25 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    clock = pygame.time.Clock
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        screen.fill("black", rect=None, special_flags=0)
+        screen.fill("black")
+       # print("Drawing player...")
+        player.draw(screen)
         pygame.display.flip()
-    
+        
     # Set framerate to 60 FPS
     dt = clock.tick(60) / 1000
 
