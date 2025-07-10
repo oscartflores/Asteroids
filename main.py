6cb05434e39292da176142a2bb372b08c083c042
oscartflores <oflores1502@gmail.com>
2025-07-10 13:11:29 -0700

    Classes added

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black", rect=None, special_flags=0)
         pygame.display.flip()
     
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # Set framerate to 60 FPS
+    dt = clock.tick(60) / 1000
 
 
 
